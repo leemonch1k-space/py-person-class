b365a959fd5f06eeb98e8d1d4e63d77b24b7fe40
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-10-25 13:55:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        if person.get("name") not in Person.people:
+            Person(person.get("name"), person.get("age"))
+
+    for person in people:
+        method_person = Person.people[person["name"]]
+        if person.get("husband") is not None:
+            method_person.husband = Person.people[person["husband"]]
+        if person.get("wife") is not None:
+            method_person.wife = Person.people[person["wife"]]
+
+    return [Person.people[person["name"]] for person in people]
